596168980b4f102567cee2e08e8da9719bdf4924
Ruben <ruben.buchinger89@gmail.com>
2025-08-07 12:31:42 +0200

    Jede dritte Ebene die wieder auf der gleichen position liegt hat eine eigene Farbe

--- FCC-Visualisierung.py
+++ FCC-Visualisierung.py
@@ -5,8 +5,9 @@ import pyvista as pv
 import tkinter as tk
 from tkinter import simpledialog
 
+# Funktion zur Generierung der FCC Gitterpunkte
+# a: Gitterkonstante, n: Anzahl der Einheiten in jeder Richtung
 def generate_fcc(a, n):
-    # Erzeugt ein FCC Gitter mit Gitterkonstante a und n Einheiten in jeder Richtung
     points = []
     for i in range(n):
         for j in range(n):
@@ -18,20 +19,33 @@ def generate_fcc(a, n):
                 points.append(np.array([i * a, j * a + a/2, k * a + a/2]))
     return np.array(points)
 
+# Funktion zur Visualisierung des FCC Gitters mit PyVista
 def plot_fcc_pyvista(points, a):
     r = (np.sqrt(2) * a) / 4
     plotter = pv.Plotter()
+    colors = []
     for p in points:
+        # Prüfe Ebenenzugehörigkeit
+        val= p[0] + p[1] + p[2]
+        m= int(round(val/a))
+        if m % 3 == 0:
+            color = 'red'
+        elif (m-1) % 3 == 0:
+            color = 'blue'
+        elif (m-2) % 3 == 0:
+            color = 'green'
+        else:
+            color = 'gray'
         sphere = pv.Sphere(radius=r, center=p)
-        plotter.add_mesh(sphere, color='red', opacity=1)
+        plotter.add_mesh(sphere, color=color, opacity=1)
     plotter.show()
 
 # --- Eingabefenster für a und n ---
 root = tk.Tk()
 root.withdraw()  # Hauptfenster ausblenden
 
-a = simpledialog.askfloat("Gitterkonstante", "Wert für a (z.B. 1.0):", minvalue=0.01)
-n = simpledialog.askinteger("Gittergröße", "Wert für n (z.B. 5):", minvalue=1)
+a = simpledialog.askfloat("Gitterkonstante", "Länge der Gitterkonstante:", minvalue=0.01)
+n = simpledialog.askinteger("Atomanzahl in jede Richtung", "Atomanzahl in jede Richtung:", minvalue=1)
 
 root.destroy()
 
