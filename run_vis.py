ab408447bc69afdd6e8c81b699240fa00dc2e7f6
Ruben <ruben.buchinger89@gmail.com>
2026-02-27 19:47:29 +0100

    Use interactive PyVista viewer as application entrypoint

--- run_vis.py
+++ run_vis.py
@@ -7,7 +7,7 @@ SRC = ROOT / "src"
 if str(SRC) not in sys.path:
     sys.path.insert(0, str(SRC))
 
-from vis.gui import run_gui
+from vis.interactive_viewer import run_interactive_viewer
 
 if __name__ == "__main__":
-    run_gui()
+    run_interactive_viewer()
